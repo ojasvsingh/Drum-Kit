1a4fed3d5a423306ceb01b13110037741a2f9b31
ojasvsingh <123519056+ojasvsingh@users.noreply.github.com>
2024-03-20 09:24:45 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+drumList = document.querySelectorAll(".drum")
+
+for(var i=0; i< drumList.length; i++ ){
+    drumList[i].addEventListener("click", function(){
+        var buttonHTML = this.innerHTML;
+        makeSound(buttonHTML);
+        buttonAnimation(buttonHTML);
+
+    });
+}
+
+document.addEventListener("keydown", function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+} );
+
+function makeSound(key){
+    switch(key){
+        case "w":
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+        break;
+        case "a":
+            var tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+        break;
+        case "s":
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+        case "d":
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+        break;
+        case "j":
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
+        break;
+        case "k":
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+        break;
+        case "l":
+            var kickbass = new Audio('sounds/kick-bass.mp3');
+            kickbass.play();
+        break;
+        default:
+            console.log(buttonClick);
+    }
+}
+
+function buttonAnimation(currentKey){
+    var activeKey = document.querySelector("."+ currentKey);
+    activeKey.classList.add("pressed");
+
+    setTimeout(function(){
+        activeKey.classList.remove("pressed")
+    },100);
+}
+
